cb9e33d5957df1524945292c12eff9384e4b4e65
rsalamow <rsalamow@gmail.com>
2025-04-15 11:20:09 +0300

    updated the code for old verion

--- Rsalamow.Pdf.Activities/Rsalamow.Pdf.Activities/ExtractPdfAttachments.cs
+++ Rsalamow.Pdf.Activities/Rsalamow.Pdf.Activities/ExtractPdfAttachments.cs
@@ -9,9 +9,6 @@ namespace RSalamow.Pdf
 {
     public class ExtractPdfAttachments : CodeActivity // This base class exposes an OutArgument named Result
     {
-        /*
-         * The returned value will be used to set the value of the Result argument
-         */
         [Category("Input")]
         [RequiredArgument]
         public InArgument<string> FilePath { get; set; }
@@ -21,78 +18,47 @@ namespace RSalamow.Pdf
 
         protected override void Execute(CodeActivityContext context)
         {
-            var files = new List<string>();
-            var filePath = FilePath.Get(context);
-            var fileFolderPath = Path.GetDirectoryName(filePath);
-
-            // Load the PDF document
-            var pdfBytes = File.ReadAllBytes(filePath);
-            using var loadedDocument = new PdfLoadedDocument(pdfBytes);
-
-            // Extract annotations
-            ExtractAnnotations(loadedDocument, files);
-
-            // Extract attachments
-            ExtractAttachments(loadedDocument, fileFolderPath, files);
-
-            Files.Set(context, files);
-        }
-
-        private void ExtractAnnotations(PdfLoadedDocument loadedDocument, List<string> files)
-        {
-            var page = loadedDocument.Pages[0] as PdfLoadedPage;
-            var annotations = page.Annotations;
-
+            List<string> files = new List<string>();
+            string filePath = FilePath.Get(context);
+            string fileFolderPath = Path.GetDirectoryName(filePath);
+            // Get current working directory
+            string currentDirectory = Directory.GetCurrentDirectory();
+            //Load the PDF document
+            byte[] pdfBytes = File.ReadAllBytes(filePath);
+            PdfLoadedDocument loadedDocument = new PdfLoadedDocument(pdfBytes);
+            //Get first page from document
+            PdfLoadedPage page = loadedDocument.Pages[0] as PdfLoadedPage;
+            //Get the annotation collection from pages
+            PdfLoadedAnnotationCollection annotations = page.Annotations;
+            //Iterates the annotations
             foreach (PdfLoadedAnnotation annot in annotations)
             {
-                if (annot is PdfLoadedAttachmentAnnotation attachmentAnnotation)
+                //Check for the attachment annotation
+                if (annot is PdfLoadedAttachmentAnnotation)
                 {
-                    var filePath = Path.Combine(Path.GetTempPath(), attachmentAnnotation.FileName);
-                    SaveAttachmentToFile(filePath, attachmentAnnotation.Data);
-                    files.Add(filePath);
+                    PdfLoadedAttachmentAnnotation file = annot as PdfLoadedAttachmentAnnotation;
+                    //Extracts the attachment and saves it to the disk
+                    FileStream stream = new FileStream(file.FileName, FileMode.Create);
+                    stream.Write(file.Data, 0, file.Data.Length);
+                    stream.Dispose();
                 }
             }
-        }
-
-        private void ExtractAttachments(PdfLoadedDocument loadedDocument, string fileFolderPath, List<string> files)
-        {
-            foreach (PdfAttachment attachment in loadedDocument.Attachments)
+            //Iterates through the attachments
+            if (loadedDocument.Attachments.Count != 0)
             {
-                var fullPath = Path.Combine(fileFolderPath, attachment.FileName);
-                fullPath = GetUniqueFilePath(fullPath);
-                SaveAttachmentToFile(fullPath, attachment.Data);
-                files.Add(fullPath);
+                foreach (PdfAttachment attachment in loadedDocument.Attachments)
+                {
+                    string fullPath = fileFolderPath + "\\" + attachment.FileName;
+                    //Extracts the attachment and saves it to the disk
+                    FileStream stream = new FileStream(fullPath, FileMode.Create);
+                    stream.Write(attachment.Data, 0, attachment.Data.Length);
+                    files.Add(fullPath);
+                    stream.Dispose();
+                }
             }
-        }
-
-        private void SaveAttachmentToFile(string filePath, byte[] data)
-        {
-            using var stream = new FileStream(filePath, FileMode.Create);
-            stream.Write(data, 0, data.Length);
-        }
-
-        private string GetUniqueFilePath(string filePath)
-        {
-            var directory = Path.GetDirectoryName(filePath);
-            var fileName = Path.GetFileNameWithoutExtension(filePath);
-            var extension = Path.GetExtension(filePath);
-            var uniqueFilePath = filePath;
-            var counter = 1;
-
-            while (File.Exists(uniqueFilePath))
-            {
-                uniqueFilePath = Path.Combine(directory, $"{fileName}({counter}){extension}");
-                counter++;
-            }
-
-            return uniqueFilePath;
-        }
-
-        public void ExecuteInternal()
-        {
-            // Use this to automatically attach the debugger to the process
-            // Debugger.Launch();
-            throw new NotImplementedException();
+            //Close the document
+            loadedDocument.Close(true);
+            Files.Set(context, files);
         }
     }
 }
